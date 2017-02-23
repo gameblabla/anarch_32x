8eb67a8048572df58796705d700c2500f8f20dc1
Gameblabla <gameblabla@openmailbox.org>
2017-02-23 17:34:09 -0500

    first commit

---
+++ image.s
@@ -0,0 +1,14 @@
+.text
+.globl _title
+.globl _ex
+.globl _pal
+.globl _pal_end
+
+
+_title:
+.incbin "sex.apx"
+_ex:
+.incbin "ex.apx"
+_pal:
+.incbin "sex.pal"
+_pal_end:
---
+++ tool/build.sh
@@ -0,0 +1,2 @@
+./sixpack -image -target 32x -format l8 -q 256 sex.png -o sex.apx
+./sixpack -image -target 32x -format l8 -q 256 ex.png -o ex.apx
